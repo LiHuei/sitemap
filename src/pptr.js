c8b74fbcfd20d57ee7b70af3fe2f8b444b48ee65
li_huei <hccpaps40832@gmail.com>
2023-08-12 22:24:14 +0800

    add pptr.ts

---
+++ src/pptr.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+
+const WebService = {
+  async run(url) {
+    try {
+      const browser = await puppeteer.launch({ headless: "new" });
+      const page = await browser.newPage();
+
+      // Set screen size
+      await page.setViewport({ width: 1080, height: 1024 });
+
+      // Navigate the page to a URL
+      await page.goto(url, { waitUntil: "networkidle0" });
+
+      const links = await page.$$eval("a", (collection) =>
+        collection.map((ele) => ele.href)
+      );
+      const imgs = await page.$$eval("img", (collection) =>
+        collection.map((ele) => ele.src)
+      );
+
+      await browser.close();
+
+      return { links, imgs };
+    } catch (error) {
+      return { links: [], imgs: [] };
+    }
+  },
+};
+
+module.exports = WebService;
